86220de45d8fcc118996fe10520aea5a366a1c5e
DreamAutumn <1796934407@qq.com>
2024-06-06 15:19:57 +0800

    功能测试

--- functinal_tests.py
+++ functinal_tests.py
@@ -3,4 +3,6 @@ from selenium import webdriver
 browser = webdriver.Chrome()
 browser.get('http://localhost:8080')
 
-assert 'Django' in browser.page_source
+assert 'To-Do' in browser.title
+
+browser.quit()
